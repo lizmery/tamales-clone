c7703dbe038f6df8ae8821328dd0d3186db35d9a
lizmery <l.vigil096@gmail.com>
2022-02-16 20:50:55 -0800

    Create Contact.jsx

---
+++ src/pages/About/sections/Contact.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import tamales from '../../../assets/tamales4.jpeg'
+
+const Quote = () => {
+    return (
+        <div className="contact">
+            <div className="contact-img">
+                <img src={tamales} alt="tamales" />
+            </div>
+            <form>
+                <h6 className="subheading">Get in touch</h6>
+                <h1 className="mission-heading heading">Contact Us</h1>
+                <div className="inputBox">
+                    <input type="text" name="name" required />
+                    <label for="name">Name</label>
+                </div>
+                <div className="inputBox">
+                    <input type="number" name="phone" required />
+                    <label for="phone">Phone</label>
+                </div>
+                <div className="inputBox">
+                    <input type="email" name="email" required />
+                    <label for="email">Email</label>
+                </div>
+                <div className="inputBox">
+                    <textarea name="mssg" id="message" cols="30" rows="10" />
+                    <label for="mssg">Message</label>
+                </div>
+                <input type="submit" class="contact-btn" value="Send Message" />
+            </form>
+
+        </div>
+    )
+}
+
+export default Quote
