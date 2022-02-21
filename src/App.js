38cb31c7357d89ac551a594ab0e036d6b0006fdd
lizmery <l.vigil096@gmail.com>
2022-02-21 14:40:26 -0800

    Update Navbar.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home/Home';
@@ -6,12 +7,57 @@ import About from './pages/About/About';
 import Menu from './pages/Menu/Menu';
 import Footer from './components/Footer/Footer';
 import './App.css'
+import Cart from './components/Cart/Cart';
 
 const App = () => {
+  const [products, setProducts] = useState([])
+  const [cart, setCart] = useState({})
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list()
+
+    setProducts(data)
+  }
+
+  const fetchCart = async () => {
+    const cart = await commerce.cart.retrieve()
+
+    setCart(cart)
+  }
+
+  const handleAddToCart = async (productId, quantity) => {
+    const { cart } = await commerce.cart.add(productId, quantity)
+
+    setCart(cart)
+  }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, {quantity})
+
+    setCart(cart)
+  }
+
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId)
+
+    setCart(cart)
+  }
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty()
+
+    setCart(cart)
+  }
+
+  useEffect(() => {
+    fetchProducts()
+    fetchCart()
+  }, [])
+
   return (
     <>
       <Router>
-        <Navbar />
+        <Navbar totalItems={cart.total_items} />
         <Routes>
           <Route
             path="/"
@@ -23,7 +69,18 @@ const App = () => {
           />
           <Route
             path="/menu"
-            element={<Menu />}
+            element={<Menu products={products} onAddToCart={handleAddToCart} />}
+          />
+          <Route 
+            path="/cart"
+            element={
+              <Cart
+                cart={cart}
+                handleUpdateCartQty={handleUpdateCartQty}
+                handleRemoveFromCart={handleRemoveFromCart}
+                handleEmptyCart={handleEmptyCart} 
+              />
+            }
           />
         </Routes>
         <Footer />
